41498c56e450a49e448354ae5cec03f62320af41
laevad <77134175+laevad@users.noreply.github.com>
2023-06-03 15:17:39 +0800

    setup configuration

--- src/components/UploadForm.tsx
+++ src/components/UploadForm.tsx
@@ -1,10 +1,32 @@
+import React, {useState} from "react";
+
 export const UploadForm = () => {
+	const [selectedFile, setSelectedFile] = useState<File | null>(null)
+
+	const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		if (e.target.files && e.target.files[0]) {
+			setSelectedFile(e.target.files[0]);
+		}
+	}
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		if (selectedFile) {
+			// upload file
+		}
+		setSelectedFile(null);
+	}
+
 	return (
 		<>
 			<div className={"text-center mt-10"}>
-				<form action="" className={"flex items-center flex-col gap-8"}>
-					<input type="file" className="file-input file-input-bordered w-full max-w-xs" />
-					<button className={"btn gap-3"}>
+				<form
+					onSubmit={handleSubmit}
+					action="" className={"flex items-center flex-col gap-8"}>
+					<input type="file" className="file-input file-input-bordered w-full max-w-xs"
+					onChange={handleFileChange}
+					/>
+					<button type={"submit"} className={"btn gap-3"}>
 						Upload <span>🚀</span>
 					</button>
 				</form>
