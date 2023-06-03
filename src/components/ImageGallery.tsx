2accf3aae8f74510c461c81ce036181a1aa0550e
laevad <77134175+laevad@users.noreply.github.com>
2023-06-03 13:10:45 +0800

    design the ImageGallery.tsx

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -1,8 +1,18 @@
 export const ImageGallery = () => {
 	return (
 		<>
-		<div>
-			<h1>Image Gallery</h1>
+		<div className={"grid md:grid-cols-3 justify-center gap-4 mt-10"}>
+			<div className="card card-compact w-96 bg-base-100 shadow-xl">
+				<figure><img src="" alt="Shoes" /></figure>
+				<div className="card-body">
+					<p>
+						Uploaded by <a href="#">@username</a>
+					</p>
+					<span>
+						Created on <time>2021-09-01</time>
+					</span>
+				</div>
+			</div>
 		</div>
 		</>
 	)
