d90b8b004f425b9fbfe671f09a65b2047fb60522
laevad <77134175+laevad@users.noreply.github.com>
2023-06-03 12:58:17 +0800

    design the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,14 @@ export const Navbar = () => {
 	return (
 		<>
 			<div>
-				<h1>Navbar</h1>
+				<div className="navbar bg-base-100 justify-between">
+					<a className="font-bold normal-case text-xl underline">
+						Laevad Gallery
+					</a>
+					<button>
+						Logout
+					</button>
+				</div>
 			</div>
 		</>
 	)
